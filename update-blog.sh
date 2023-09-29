663fde6c8cb8352023d92c0415dba13d97396aaa
Mayco <mayco.s.berghetti@gmail.com>
2023-09-28 21:00:51 -0400

    add script to udpate blog to cloud

---
+++ update-blog.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# fetch changes made online (i.e stackedit.io)
+git pull
+
+# generate static files in public folder
+pushd blog
+hugo
+popd
+
+#push public folder to cloud (i.e github)
+pushd blog/public
+git add .
+git commit -m "update blog"
+git push
+popd
