6f6eced26abdab16f9f9129cdd74789415225ccb
Mayco <mayco.s.berghetti@gmail.com>
2023-10-02 19:20:53 -0400

    .

--- update-blog.sh
+++ update-blog.sh
@@ -2,16 +2,19 @@
 
 #
 # fetch changes made online (i.e stackedit.io)
-git pull
+echo "Update from guthub"
+git pull > /dev/null
 
 # generate static files in public folder
-pushd blog
-hugo
+echo "Creating files in public folder"
+pushd blog > /dev/null
+hugo > /dev/null
 popd
 
 #push public folder to cloud (i.e github)
+echo "Pushing blog in guthub"
 pushd blog/public
 git add .
 git commit -m "update blog"
-git push
+git push > /dev/null
 popd
